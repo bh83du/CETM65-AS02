15d8ac9bb05ce15d7110dd4eb1d20a51bfdc19d2
bh83du <bh83du@student.sunderland.ac.uk>
2021-01-04 14:22:08 +0000

    Added App to settings.py. Added templates and initial HTML

--- teamkb/knowledgebase/views.py
+++ teamkb/knowledgebase/views.py
@@ -1,11 +1,32 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Initial landing response for Home page.
-def home(request):
-    return HttpResponse('<h1>Knowledge Base Home</h1>')
+# Dummy post data - Added for initial testing
+posts = [
+    {
+        'author': 'Stuart',
+        'title': 'First Post',
+        'content': 'This is the content of the first post',
+        'date_posted': 'January 3rd, 2021',
 
-# Add an 'About' Page
+    },
+    {
+        'author': 'Mike',
+        'title': 'Second Post',
+        'content': 'This is the content of the second post',
+        'date_posted': 'January 4th, 2021',
+
+    }
+]
+
+# Updated view to render home.html
+def home(request):
+    context = {
+        'posts': posts
+    }
+    return render(request, 'knowledgebase/home.html', context)
+
+# Updated view to render about.html
 
 def about(request):
-    return HttpResponse('<h1>Knowledge Base About</h1>')
+    return render(request, 'knowledgebase/about.html', {'title': 'About'})
